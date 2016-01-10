703e2ea4a75d1183181afc4f3858a78e7fe5a195
cawara <cawa.rar@gmail.com>
2016-01-11 02:52:09 +0900

    タイトル画面を追加

---
+++ Assets/Scripts/TileCubeBehavior.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileCubeBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (0f, Time.deltaTime * 10.0f, 0f));
+		transform.Rotate (new Vector3 (Time.deltaTime * 12.0f, 0f,  0f));
+		transform.Rotate (new Vector3 (0f, 0f, Time.deltaTime * 3.0f));
+	}
+
+}
